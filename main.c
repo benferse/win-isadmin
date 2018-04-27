613ab0eb3b10ad1b42130b7c79f99ce65a83a573
Ben Fersenheim <benferse@microsoft.com>
2018-04-27 17:52:09 -0400

    Refactor a touch. I guess I'll free that alloc, meh

--- main.c
+++ main.c
@@ -9,24 +9,25 @@
 int main (int argc, char** argv)
 {
     BOOL bTokenIsAdmin = FALSE;
+    HANDLE hProcessHeap = GetProcessHeap();
     DWORD cbSid = SECURITY_MAX_SID_SIZE;
-    const PSID pSid = HeapAlloc(GetProcessHeap(), 0, cbSid);
+    const PSID pSid = HeapAlloc(hProcessHeap, 0, cbSid);
+    int exitStatus = -1;
 
     UNREFERENCED_PARAMETER(argc);
     UNREFERENCED_PARAMETER(argv);
 
-    if (!CreateWellKnownSid(WinBuiltinAdministratorsSid, NULL, pSid, &cbSid))
+    if (CreateWellKnownSid(WinBuiltinAdministratorsSid, NULL, pSid, &cbSid))
     {
-        return -1;
+        if (CheckTokenMembership(NULL, pSid, &bTokenIsAdmin))
+        {
+            // To keep with exit status conventions, return 0 if the user
+            // is running as an administrator (i.e. the check succeeded),
+            // non-zero otherwise.
+            exitStatus = !bTokenIsAdmin;
+        }
     }
 
-    if (!CheckTokenMembership(NULL, pSid, &bTokenIsAdmin))
-    {
-        return -1;
-    }
-
-    // To keep with exit status conventions, return 0 if the user
-    // is running as an administrator (i.e. the check succeeded),
-    // non-zero otherwise.
-    return !bTokenIsAdmin;
+    HeapFree(hProcessHeap, 0, pSid);
+    return exitStatus;
 }
